58032fb9b456b84f5f79d26fcf2836f078cd82b8
Deepak Verma <dpakverma789@gmail.com>
2024-07-04 10:27:07 +0530

    resume structural improvement

--- src/Section_Right.js
+++ src/Section_Right.js
@@ -1,7 +1,7 @@
 import './resume.css';
 
 function PROJECTS(props){
-    const projects_list = props.project.projects
+    const projects_list =  props.techProjects.arguments.projects;
     const itemList = projects_list.map((project,i)=>
         <>
             <div className={'project_'+i}>
@@ -12,66 +12,44 @@ function PROJECTS(props){
         </>
     );
     return(itemList);
-
 }
 
 
+function TECH_SKILLS(props) {
+    const TechList = props.techSkills.arguments.TechGraphic;
+    const techSkills = TechList.map((skill) => 
+        <>
+            <div className='tech-skill row col-6'>
+            <i className= {skill.icon+' skill-fa col-3'}></i>
+                <div className="progress col-9">
+                    <div className="progress-bar" role="progressbar" style={{ width: skill.widthPx }}
+                        aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </>
+    )
+    return (techSkills);
+}
+
+
+function TOOL_LIST(props) {
+    const ToolList = props.toolList.arguments.ToolList;
+    const Tool_List = ToolList.map((tool) => 
+        <>
+            <div className={tool.CoLSize}>{tool.ColVal}</div>
+        </>
+    )
+    return (Tool_List);
+}
+
 function SECTION_RIGHT(props) {
-    
     return (
         <>
             <section className="right" id="right">
                 <div className='tech-skills'>tech Skills</div>
                 <div className='seperator-right'/>
                 <div className='row'>
-                    {/* HTML */}
-                    <div className='tech-skill row col-6'>
-                        <i className="fa-brands fa-html5 skill-fa col-3"></i>
-                        <div className="progress col-9">
-                            <div className="progress-bar" role="progressbar" style={{ width: 50 }}
-                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    {/* Python */}
-                    <div className='tech-skill row col-6'>
-                        <i className="fa-brands fa-python skill-fa col-3"></i>
-                        <div className="progress col-9">
-                            <div className="progress-bar" role="progressbar" style={{ width: 40 }}
-                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    {/* CSS */}
-                    <div className='tech-skill row col-6'>
-                        <i className="fa-brands fa-css3-alt skill-fa col-3"></i>
-                        <div className="progress col-9">
-                            <div className="progress-bar" role="progressbar" style={{ width: 50 }}
-                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    {/* React */}
-                    <div className='tech-skill row col-6'>
-                        <i className="fa-brands fa-react skill-fa col-3"></i>
-                        <div className="progress col-9">
-                            <div className="progress-bar" role="progressbar" style={{ width: 30 }}
-                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    {/* JS */}
-                    <div className='tech-skill row col-6'>
-                        <i className="fa-brands fa-square-js skill-fa col-3"></i>
-                        <div className="progress col-9">
-                            <div className="progress-bar" role="progressbar" style={{ width: 30 }}
-                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    {/* Database */}
-                    <div className='tech-skill row col-6'>
-                        <i className="fa-solid fa-database skill-fa col-3" style={{ color: "black" }}></i>
-                        <div className="progress col-9">
-                            <div className="progress-bar" role="progressbar" style={{ width: 50 }}
-                                aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
+                    <TECH_SKILLS techSkills = {props}/>
                 </div>
                 <br />
                 <br />
@@ -87,21 +65,13 @@ function SECTION_RIGHT(props) {
                 <div className='tech-tools'>used Tools</div>
                 <div className='seperator-right'/>
                 <div className='tools-list row'>
-                    <div className='col-3'>pycharm</div>
-                    <div className='col-3'>vscode</div>
-                    <div className='col-2'>git</div>
-                    <div className='col-3'>postgres</div>
-                    <div className='col-2'>jira</div>
-                    <div className='col-2'>figma</div>
-                    <div className='col-3'>postman</div>
-                    <div className='col-3'>ubuntu</div>
-                    <div className='col-3'>windows</div>
+                   <TOOL_LIST toolList = {props}/>
                 </div>
                 <br />
                 <div className='tech-projects'>projects</div>
                 <div className='seperator-right'/>
                 <div className='project-list'>
-                    <PROJECTS project={props}/>
+                    <PROJECTS techProjects = {props}/>
                 </div>
             </section>
         </>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SECTION_LEFT from './Section_Left';
 import SECTION_RIGHT from './Section_Right';
-import {HEADER,  DOWNLOAD_BUTTON} from './Header.js';
+import { HEADER, DOWNLOAD_BUTTON } from './Header.js';
 
 const user_details = {
   "name": "deepak verma",
@@ -12,90 +12,158 @@ const user_details = {
   "address": "udaipur, rajasthan"
 }
 
-const info = 
-  {
-    "education": [
-      {
-        "course": "master of business administration (I.T)",
-        "university": "narsee monjee inst. of management",
-        "duration": "sept 2021 - oct 2023",
-        "location": "mumbai, MH"
-      },
-      {
-        "course": "b.tech (electronics & communication)",
-        "university": "rajasthan technical university",
-        "duration": "aug 2015 - june 2019",
-        "location": "jaipur, RJ"
-      }
-    ],
+const info = {
+  "education": [
+    {
+      "course": "master of business administration (I.T)",
+      "university": "narsee monjee inst. of management",
+      "duration": "sept 2021 - oct 2023",
+      "location": "mumbai, MH"
+    },
+    {
+      "course": "b.tech (electronics & communication)",
+      "university": "rajasthan technical university",
+      "duration": "aug 2015 - june 2019",
+      "location": "jaipur, RJ"
+    }
+  ],
 
-    "experience": [
-      {
-        "company": "logicloop venture pvt. ltd.",
-        "designation": "software engineer",
-        "duration": "April 2022 - Present",
-        "location": "mumbai, MH",
-        "responsibility": [
-          "Hands on odoo ERP python (MVC) framework",
-          "Tech used odoo ORM, postgresDB, XMl, JS, APIs",
-          "Managing inhouse odoo modules"
-        ]
-      },
-    
-      {
-        "company": "ksolves india pvt. ltd.",
-        "designation": "software engineer",
-        "duration": "Sept 2020 - March 2022",
-        "location": "noida, UP",
-        "responsibility": [
-          "Hands on odoo ERP python (MVC) framework",
-          "Tech used odoo ORM, postgresDB, XMl, JS, APIs",
-          "Odoo modules: sales, purchase, crm, website, accounts, inventory"
-        ]
-      },
+  "experience": [
+    {
+      "company": "logicloop venture pvt. ltd.",
+      "designation": "software engineer",
+      "duration": "April 2022 - Present",
+      "location": "mumbai, MH",
+      "responsibility": [
+        "Hands on odoo ERP python (MVC) framework",
+        "Tech used odoo ORM, postgresDB, XMl, JS, APIs",
+        "Managing inhouse odoo modules"
+      ]
+    },
 
-      {
-        "company": "career transition",
-        "designation": "career break (covid-lockdown)",
-        "duration": "april 2020 - aug 2020",
-        "location": "udaipur, RJ",
-        "responsibility": []
-      },
+    {
+      "company": "ksolves india pvt. ltd.",
+      "designation": "software engineer",
+      "duration": "Sept 2020 - March 2022",
+      "location": "noida, UP",
+      "responsibility": [
+        "Hands on odoo ERP python (MVC) framework",
+        "Tech used odoo ORM, postgresDB, XMl, JS, APIs",
+        "Odoo modules: sales, purchase, crm, website, accounts, inventory"
+      ]
+    },
 
-      {
-        "company": "AVA Consulting",
-        "designation": "US IT Recruiter",
-        "duration": "Sept 2019 - March 2020",
-        "location": "noida, UP",
-        "responsibility": []
-      }
-    ]
+    {
+      "company": "career transition",
+      "designation": "career break (covid-lockdown)",
+      "duration": "april 2020 - aug 2020",
+      "location": "udaipur, RJ",
+      "responsibility": []
+    },
+
+    {
+      "company": "AVA Consulting",
+      "designation": "US IT Recruiter",
+      "duration": "Sept 2019 - March 2020",
+      "location": "noida, UP",
+      "responsibility": []
+    }
+  ]
 }
 
-const projects = [
-  'https://managerpass.vercel.app',
-  'https://readzara.vercel.app',
-  'https://django-project-apis.vercel.app',
-  'https://github.com/dpakverma789/mini-projects',
-  'https://github.com/dpakverma789/Odoo'
-]
 
- 
+const RightSectionArguments = {
+  "projects": [
+    'https://managerpass.vercel.app',
+    'https://readzara.vercel.app',
+    'https://django-project-apis.vercel.app',
+    'https://github.com/dpakverma789/mini-projects',
+    'https://github.com/dpakverma789/Odoo'
+  ],
+
+  "TechGraphic": [
+    {
+      "icon": "fa-brands fa-html5",
+      "widthPx": 50
+    },
+    {
+      "icon": "fa-brands fa-python",
+      "widthPx": 40
+    },
+    {
+      "icon": "fa-brands fa-css3-alt",
+      "widthPx": 50
+    },
+    {
+      "icon": "fa-brands fa-react",
+      "widthPx": 30
+    },
+    {
+      "icon": "fa-brands fa-square-js",
+      "widthPx": 30
+    },
+    {
+      "icon": "fa-solid fa-database",
+      "widthPx": 50
+    }
+  ],
+
+  "ToolList": [
+    {
+      "CoLSize": "col-3",
+      "ColVal": "pycharm"
+    },
+    {
+      "CoLSize": "col-3",
+      "ColVal": "vscode"
+    },
+    {
+      "CoLSize": "col-2",
+      "ColVal": "git"
+    },
+    {
+      "CoLSize": "col-3",
+      "ColVal": "postgres"
+    },
+    {
+      "CoLSize": "col-2",
+      "ColVal": "jira"
+    },
+    {
+      "CoLSize": "col-2",
+      "ColVal": "figma"
+    },
+    {
+      "CoLSize": "col-3",
+      "ColVal": "postman"
+    },
+    {
+      "CoLSize": "col-3",
+      "ColVal": "ubuntu"
+    },
+    {
+      "CoLSize": "col-3",
+      "ColVal": "windows"
+    }
+  ]
+}
 
 
 const download_button = ReactDOM.createRoot(document.getElementById('download-button'));
 download_button.render(
   <>
-      <DOWNLOAD_BUTTON/>
+    <DOWNLOAD_BUTTON />
   </>
 );
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <HEADER user={user_details} />
     <div className='section-container'>
-      <SECTION_LEFT user_info={ info } />
-      <SECTION_RIGHT projects={ projects }/>
+      <SECTION_LEFT user_info={info} />
+      <SECTION_RIGHT arguments={RightSectionArguments} />
     </div>
   </>
 )
